c8b1272318aeb5aeab370cce61db53138940f361
Matthew Nielsen <xunker@pyxidis.org>
2015-02-09 12:08:45 -0700

    Select ebook product variant before adding to card when doing a digital purchase.

--- tests/helpers/base_checkout_helper.rb
+++ tests/helpers/base_checkout_helper.rb
@@ -10,13 +10,17 @@ module BaseCheckoutHelper
   def add_digital_item_to_cart
     goto "/products/romney-family-table-ann-89648?variant_id=5897"
 
+    # select the eBook product variant
+    browser.div(id: 'product-variants').a(title: 'eBook').click
     browser.button(id: "add-to-cart-button").click
     assert browser.text.include?("Item added to cart"), "Item was not correctly added to cart"
   end
 
-   def begin_checkout
+  def begin_checkout
     goto "/cart"
     browser.button(id: "checkout-link").click
+    # give time for next page to load before continuing
+    browser.button(id: "checkout-link").wait_while_present
   end 
 
   def select_delivery

6fde77b1eb302451a1a284cde1bc0f541386ba75
Matthew Nielsen <xunker@pyxidis.org>
2015-09-10 17:36:25 -0600

    Added test for header menus and all the links therein.

--- tests/home_page_test.rb
+++ tests/home_page_test.rb
@@ -1,5 +1,6 @@
 require 'test_helper'
 require 'httparty'
+require 'ruby-progressbar'
 
 class HomePageTest < NibleyTest
   # Make sure all links on home page go to a URL that returns a 200 OK response.
@@ -8,7 +9,7 @@ class HomePageTest < NibleyTest
   # with a "#").
   # Note that we only use webdriver to load the home page; all of the links
   # are then tested using HTTParty because it's faster.
-  def test_all_links_return_200_while_not_logged_in
+  def test_all_links_return_200
     goto '/'
     anchors = browser.driver.find_elements(tag_name: 'a').select(&:displayed?).select do |anchor|
       href = anchor.attribute('href').to_s
@@ -35,7 +36,6 @@ class HomePageTest < NibleyTest
 
     failures = []
 
-    require 'ruby-progressbar'
     progressbar = ProgressBar.create(
       title: 'Checking URLs',
       total: anchors.length,
@@ -47,13 +47,7 @@ class HomePageTest < NibleyTest
       text = anchor.text.inspect
       # puts "Checking link #{i+1}/#{anchors.size}: #{text} (#{url.inspect})"
 
-      # GET or HEAD? Head is more friendly to the server, but some pages
-      # May behave differently depending on HEAD or GET.
-      response = HTTParty.head(url,
-        verify: false, # don't verify ssl certs
-      )
-
-      failures << anchor unless response.code == 200
+      failures << anchor unless valid_link?(url)
 
       progressbar.increment
     end
@@ -68,5 +62,76 @@ class HomePageTest < NibleyTest
       debug_on_failure: false
     )
 
+  end
+
+  # Go through all the links in the drop-down menus of the home page header and
+  # make sure they all go to a page that returns 200 OK.
+  def test_all_header_menu_links_return_200
+    goto '/'
+
+    failures = []
+
+    browser.ul(class: 'nav-links').lis.each{|menu_li|
+      next unless menu_li.attribute_value('data-url')
+
+      menu_name = menu_li.text
+      # puts menu_name.inspect
+
+      # There may be a hidden <div> in the <li>, or just a link.
+      # If it's just a link, record the anchor and continue.
+      if !menu_li.div.exists? && menu_li.a.exists?
+        unless valid_link?(menu_li.a.attribute_value('href'))
+          failures << [menu_name, menu_li.a.attribute_value('href')]
+        end
+        next
+      end
+
+      # before mouse-over, verify the div is hidden.
+      assert(!menu_li.div.visible?)
+
+      # next, mouse-over the li and verify the div is now visible
+      menu_li.hover
+      assert(menu_li.div.visible?)
+
+      # test all the links
+      menu_anchors = menu_li.div.as.to_a
+
+      progressbar = ProgressBar.create(
+        title: "Links in #{menu_name.inspect}",
+        total: menu_anchors.length,
+        format: "%t: |%B| %p%% (%c/%C) %e"
+      )
+
+      menu_anchors.each do |anchor|
+        url = anchor.attribute_value('href')
+        text = anchor.text
+        # puts "Checking link #{text.inspect} (#{url.inspect})"
+        failures << [menu_name, url, text] unless valid_link?(url)
+        progressbar.increment
+      end
+    }
+
+    assert(failures.empty?,
+      [
+        'The following links did not returns a 200:',
+        failures.map{|menu, url, text|
+          "Menu: #{menu.inspect}, Text: #{text.inspect}, URL: #{url.inspect}"
+        }.join("\n")
+      ].join("\n"),
+      debug_on_failure: false
+    )
+
+  end
+
+private
+
+  # Argument is URL as a string.
+  # Returns true if url returns a 200 OK. False if otherwise.
+  def valid_link?(url)
+    # GET or HEAD? Head is more friendly to the server, but some pages
+    # May behave differently depending on HEAD or GET.
+    HTTParty.head(url,
+      verify: false, # don't verify ssl certs
+    ).code == 200
   end
 end
